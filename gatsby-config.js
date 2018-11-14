4ad6605106f6287c2f774285e4e676deb7031e51
duncan heron <duncanuk@gmail.com>
2018-11-14 14:26:08 +0000

    adds redirects

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,6 +8,7 @@ module.exports = {
   },
   plugins: [
     'gatsby-plugin-react-helmet',
+    `gatsby-plugin-netlify`,
     `gatsby-plugin-styled-components`,
     {
       resolve: `gatsby-plugin-manifest`,
