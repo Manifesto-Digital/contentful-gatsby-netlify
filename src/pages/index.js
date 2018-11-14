21090763bf452cdc3d08f10bbf7da8291483af9a
duncan heron <duncanuk@gmail.com>
2018-11-14 10:22:59 +0000

    changes text

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,7 @@ import Layout from '../components/layout'
 const IndexPage = () => (
   <Layout>
     <h1>Hi develop</h1>
-    <p>Welcome to your new Gatsby site.</p>
+    <p>Welcome to your new Shelter Gatsby site.</p>
     <p>Now go build something great.</p>
     <Link to="/demo/">Go to demo page</Link> <br />
     <Link to="/page/">Go to page</Link>
