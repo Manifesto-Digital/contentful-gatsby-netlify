fa0a4a223113c3914a61919f0e17bfa9277e9dc2
duncan heron <duncanuk@gmail.com>
2018-11-15 17:17:04 +0000

    wip

--- src/components/hero/index.test.js
+++ src/components/hero/index.test.js
@@ -1,8 +1,14 @@
 import React from 'react'
 import renderer from 'react-test-renderer'
-import GetInTouch from './index'
+import Hero from './index'
 
 it('renders correctly', () => {
-  const tree = renderer.create(<GetInTouch />).toJSON()
+  const mockData = {
+    title: 'hello',
+    subtitle: 'subtitle',
+    blackText: true,
+    image: 'sdfgsd',
+  }
+  const tree = renderer.create(<Hero content={mockData} />).toJSON()
   expect(tree).toMatchSnapshot()
 })
