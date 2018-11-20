04af7b8f50bbb0efa811d38dda4d0ee7033100b3
Toby Bushell <toby.bushell@manifesto.co.uk>
2018-11-20 12:58:13 +0000

    Added check to make sure li's are only rendered if there is content and not empty html tags

--- src/components/richText/unordered-list.js
+++ src/components/richText/unordered-list.js
@@ -7,9 +7,12 @@ const UnorderedList = ({ content }) => {
   if (!content || content.length === 0) return null
 
   // Temporary bug on contentful that lists are nested two extra levels deep
-  const listItems = content.map((text, i) => (
-    <li key={i}>{text.content[0].content[0].value}</li>
-  ))
+  const listItems = content.map((text, i) => {
+    const itemContent = text.content[0].content[0].value
+    if (!itemContent.length > 0 || !itemContent) return null
+
+    return <li key={i}>{itemContent}</li>
+  })
 
   return <BulletedList>{listItems}</BulletedList>
 }
