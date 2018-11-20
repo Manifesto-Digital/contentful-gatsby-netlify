54e0b5d1a891fb2071d8101fd996e8557153c94c
Toby Bushell <toby.bushell@manifesto.co.uk>
2018-11-20 12:29:00 +0000

    Make image optional for hero no card

--- src/components/hero/index.js
+++ src/components/hero/index.js
@@ -11,7 +11,9 @@ const Hero = ({ content }) => {
   return (
     <HeroNoCard>
       <MediaWrapper>
-        <ResponsiveImage mobileW={700} desktopW={1500} image={image} />
+        {test && (
+          <ResponsiveImage mobileW={700} desktopW={1500} image={image} />
+        )}
       </MediaWrapper>
       <Content black={blackText}>
         <Title>{title}</Title>
