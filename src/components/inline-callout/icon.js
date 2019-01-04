192782e8d9a45462070592a4195997280303b989
Neil Dakeyne <neil_dakeyne@shelter.org.uk>
2019-01-04 14:05:07 +0000

    updated icons to correct light weight font awesome ones

--- src/components/inline-callout/icon.js
+++ src/components/inline-callout/icon.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import SpeechBubble from '../../assets/svg/icons/speech-bubble.svg'
-import Envelope from '../../assets/svg/icons/envelope.svg'
-import Download from '../../assets/svg/icons/download.svg'
-import Printer from '../../assets/svg/icons/printer.svg'
-import OpenBook from '../../assets/svg/icons/open-book.svg'
-import Information from '../../assets/svg/icons/information.svg'
-import Telephone from '../../assets/svg/icons/telephone.svg'
-import MapMarker from '../../assets/svg/icons/map-marker.svg'
+import SpeechBubble from '../../assets/svg/icons/comment-light.svg'
+import Envelope from '../../assets/svg/icons/envelope-light.svg'
+import Download from '../../assets/svg/icons/download-light.svg'
+import Printer from '../../assets/svg/icons/print-light.svg'
+import OpenBook from '../../assets/svg/icons/book-open-light.svg'
+import Exclamation from '../../assets/svg/icons/exclamation-circle-light.svg'
+import Telephone from '../../assets/svg/icons/phone-light.svg'
+import MapMarker from '../../assets/svg/icons/map-marker-alt-light.svg'
 
 import { BannerSVG } from './styles'
 
@@ -30,8 +30,8 @@ const Icon = ({ icon }) => {
     case 'open-book':
       chosenIcon = OpenBook
       break
-    case 'information':
-      chosenIcon = Information
+    case 'exclamation-mark':
+      chosenIcon = Exclamation
       break
     case 'telephone':
       chosenIcon = Telephone
