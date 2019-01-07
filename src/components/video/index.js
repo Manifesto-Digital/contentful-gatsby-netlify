bf9dce20e71f9c919113263927308a0f69928587
Callam Williams <lhocdesign@gmail.com>
2019-01-07 15:45:48 +0000

    updated react, added full youtube schema

--- src/components/video/index.js
+++ src/components/video/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import PropTypes from 'prop-types'
 import ReactPlayer from 'react-player'
 import theme from '../theme/variables'
@@ -6,6 +6,8 @@ import RichText from '../rich-text'
 import { Wrapper, VideoWrapper, BottomText } from './styles'
 
 const VideoEmbed = ({ data }) => {
+  const [videoDuration, setDuration] = useState(null)
+  const [videoURL, setVideoID] = useState(null)
   const {
     externalUrl,
     title,
@@ -23,20 +25,50 @@ const VideoEmbed = ({ data }) => {
     },
   }
 
+  const getVideoID = url => {
+    const regExp = /^.*(youtu\.be\/|v\/|u\/\w\/|embed\/|watch\?v=|&v=)([^#&?]*).*/
+    const match = url.match(regExp)
+    if (match && match[2].length === 11) {
+      setVideoID(match[2])
+    }
+  }
+
+  useEffect(() => {
+    getVideoID(externalUrl)
+  })
+
   return (
     <Wrapper theme={theme} removeMarginBottom={removeMarginBottom}>
       <h3>{title}</h3>
-      <VideoWrapper>
+      <VideoWrapper
+        itemProp="video"
+        itemScope
+        itemType="http://schema.org/VideoObject"
+      >
         <ReactPlayer
           url={externalUrl}
           config={{ ...options }}
           width="100%"
           height="100%"
           preload="true"
+          onReady={videoData => setDuration(`PT${videoData.getDuration()}S`)}
         />
         <meta itemProp="name" content={title} />
         <meta itemProp="description" content={metaDescription || title} />
         <meta itemProp="embedUrl" content={externalUrl} />
+        {videoDuration && <meta itemProp="duration" content={videoDuration} />}
+        {videoURL && externalUrl.includes('you') && (
+          <meta
+            itemProp="thumbnailURL"
+            content={`https://i.ytimg.com/vi/${videoURL}/hqdefault.jpg`}
+          />
+        )}
+        {videoURL && externalUrl.includes('you') && (
+          <meta
+            itemProp="embedURL"
+            content={`https://youtube.googleapis.com/v/${videoURL}`}
+          />
+        )}
       </VideoWrapper>
       {bottomText && (
         <BottomText>
