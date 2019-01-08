13f67ad2e02063176c44b9cafd85d2e0ae7bc347
Callam Williams <lhocdesign@gmail.com>
2019-01-08 15:47:52 +0000

    converting to uppercase to try and get this graphql call to pass

--- src/templates/page.js
+++ src/templates/page.js
@@ -84,7 +84,7 @@ export const pageQuery = graphql`
           ...BannerTopicFragment
           ...VideoTopicFragment
           ...DownloadBannerAssemblyFragment
-          ...navigationLinkBoxFragment
+          ...NavigationLinkBoxFragment
         }
       }
     }
