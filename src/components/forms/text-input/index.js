b312c1a8cd59f3a07b8582ae9bcbb107526cbddf
Cameron Martin <cameron_martin@shelter.org.uk>
2019-01-09 16:11:12 +0000

    Moved text input styles into different file.

--- src/components/forms/text-input/index.js
+++ src/components/forms/text-input/index.js
@@ -1,41 +1,6 @@
 import React from 'react';
-import styled, { css } from 'styled-components';
 import PropTypes from 'prop-types';
-import successIcon from './success.png';
-import errorIcon from './error.png';
-
-const getStyles = ({ touched, error, theme }) => {
-  if (!touched) {
-    return css`
-      border: 1px solid ${theme.palette.grey15};
-      background-color: white;
-    `;
-  }
-
-  if (error) {
-    return css`
-      border: 1px solid ${theme.palette.error};
-      background: no-repeat 97% url(${errorIcon});
-      background-color: ${theme.palette.errorLight};
-    `;
-  }
-
-  return css`
-    border 1px solid ${theme.palette.success};
-    background: no-repeat 97% url(${successIcon});
-    background-color: ${theme.palette.successLight};
-  `;
-};
-
-const Input = styled.input`
-  ${getStyles};
-  display: block;
-  background-size: 15px 15px;
-  outline: none;
-  width: 100%;
-  margin-bottom: ${props => props.theme.spacing.standard};
-  padding: ${props => props.theme.spacing.small};
-`;
+import { StyledInput } from './styles';
 
 /**
  * Renders
@@ -43,7 +8,7 @@ const Input = styled.input`
  * @param {import('formik').FieldProps} props
  */
 const TextInput = ({ field, form, ...rest }) => (
-  <Input
+  <StyledInput
     as={rest.type === 'textarea' ? 'textarea' : undefined}
     {...field}
     {...rest}
