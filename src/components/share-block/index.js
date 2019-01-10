68bcf20fb6b6c922f895a0a7f7003ccb276a4396
Callam Williams <lhocdesign@gmail.com>
2019-01-10 10:46:49 +0000

    styled up, correct format

--- src/components/share-block/index.js
+++ src/components/share-block/index.js
@@ -1,45 +1,65 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Location } from '@reach/router';
 import Icon from './icon';
 import { consistentString } from '../../utils/content-formatting';
-import { Wrapper, PrintButton } from './styles';
+import { Wrapper, PrintButton, ShareLink } from './styles';
 
 const ShareBlock = ({ data }) => {
   const { headerText, shareType } = data;
   const url = {
+    Print: 'printer',
     Email: 'mailto:?&subject=Shelter&body=',
     WhatsApp: 'https://api.whatsapp.com/send?text=',
     Twitter: 'https://twitter.com/home?status=',
     Facebook: 'https://www.facebook.com/sharer/sharer.php?u=',
   };
-  const shareUrl = url[shareType] || url.Email;
 
   return (
-    <Wrapper>
-      {headerText}
-      <PrintButton
-        onClick={() => window.print()}
-        onKeyDown={event => {
-          if (event.keycode === 13) window.print();
-        }}
-        type="button"
-        tabIndex="0"
-      >
-        Print this article
-      </PrintButton>
-      <a href={`${shareUrl}${window.location.href}`}>
-        <Icon icon={consistentString(shareType)} />
-        {shareType === 'Email' ? 'Email' : 'Share'}
-        {' this article'}
-      </a>
-    </Wrapper>
+    <>
+      <h3>{headerText}</h3>
+      <Wrapper>
+        {shareType.map(type => (
+          <>
+            {type === 'Print' ? (
+              <PrintButton
+                onClick={() => window.print()}
+                onKeyDown={event => {
+                  if (event.keycode === 13) window.print();
+                }}
+                type="button"
+                tabIndex="0"
+              >
+                <Icon icon="print" />
+                Print this article
+              </PrintButton>
+            ) : (
+              <Location>
+                {({ location }) => (
+                  <ShareLink
+                    href={`${url[type]}${location.href}`}
+                    target="_blank"
+                    rel="noopener"
+                  >
+                    {console.log(location)}
+                    <Icon icon={consistentString(type)} />
+                    {type === 'Email' ? 'Email' : 'Share'}
+                    {' this article'}
+                  </ShareLink>
+                )}
+              </Location>
+            )}
+          </>
+        ))}
+      </Wrapper>
+    </>
   );
 };
 
 ShareBlock.propTypes = {
   data: PropTypes.shape({
     headerText: PropTypes.string.isRequired,
-    shareType: PropTypes.string.isRequired,
+    shareType: PropTypes.array.isRequired,
   }),
 };
 
--- src/components/share-block/styles.js
+++ src/components/share-block/styles.js
@@ -1,35 +1,50 @@
 import styled from 'styled-components';
 import SVG from 'react-inlinesvg';
 import { breakpoint } from '../theme/breakpoint';
+import { buttonReset } from '../styled/buttons';
+import { linkStyles } from '../styled/links';
 
 export const Wrapper = styled.div`
   display: flex;
+  justify-content: space-between;
+  flex-direction: column;
   width: 100%;
-  padding: ${props => props.theme.spacing.padding};
+  margin-bottom: ${props => props.theme.spacing.standard};
+  padding: ${props => props.theme.spacing.standard};
   border: 2px solid ${props => props.theme.palette.royalBlue};
+
+  ${breakpoint.tablet`
+    flex-direction: row;
+    padding: ${props => props.theme.spacing.medium};
+  `}
 `;
 
 export const PrintButton = styled.button`
-  padding: 0;
-  background: none;
-  border: none;
-  color: inherit;
-  font: inherit;
-  cursor: pointer;
-  appearance: none;
+  ${buttonReset}
+  ${linkStyles}
+  display: flex;
+  width: 100%;
+  margin-bottom: ${props => props.theme.spacing.small};
+  text-decoration: none;
+
+  ${breakpoint.tablet`
+    margin-bottom: 0;
+  `}
+`;
+
+export const ShareLink = styled.a`
+  display: flex;
+  width: 100%;
+  text-decoration: none;
 `;
 
 export const StyledSVG = styled(SVG)`
   display: block;
+  margin-right: ${props => props.theme.spacing.standard};
   fill: ${props => props.theme.palette.royalBlue};
 
   svg {
-    width: 20px;
-    height: 20px;
-
-    ${breakpoint.tablet`
-      width: 25px;
-      height: 25px;
-    `}
+    width: 30px;
+    height: 30px;
   }
 `;
