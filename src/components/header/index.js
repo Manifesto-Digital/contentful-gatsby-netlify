cd403f0ca0c7a9406c2f33fca1144c70fd5fff43
Callam Williams <lhocdesign@gmail.com>
2019-01-14 15:52:06 +0000

    started header

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,8 +1,131 @@
 import React from 'react';
+import LogoSVG from '../../assets/svg/icons/logo.svg';
+import MenuSVG from '../../assets/svg/icons/menu.svg';
+import CloseSVG from '../../assets/svg/icons/close.svg';
+import useToggle from '../../utils/useToggle';
+import {
+  HeaderWrapper,
+  HeaderBar,
+  LogoWrapper,
+  Logo,
+  MenuControl,
+  StyledMenuSVG,
+  MenuWrapper,
+} from './styles';
 
-// Styles
-import { Wrapper } from './styles';
+const Header = () => {
+  const [isOpen, openState] = useToggle(false);
+  return (
+    <>
+      <HeaderWrapper>
+        <HeaderBar>
+          <LogoWrapper>
+            <Logo src={LogoSVG} cacheGetRequests />
+          </LogoWrapper>
+          <MenuControl type="button" onClick={openState} active={isOpen}>
+            <StyledMenuSVG src={MenuSVG} />
+          </MenuControl>
+        </HeaderBar>
+      </HeaderWrapper>
+      <MenuWrapper active={isOpen}>
+        <MenuControl type="button" onClick={openState} active={isOpen}>
+          <StyledMenuSVG src={CloseSVG} />
+        </MenuControl>
+        <ul className="row unbulleted ">
+          <li>
+            <a
+              href="https://england.shelter.org.uk/housing_advice"
+              id="test-24462"
+            >
+              Housing advice
+            </a>
+            <ul className="unbulleted subnav js-subnavbar">
+              <div className="row">
+                <li className="js-subnav js-active">
+                  <a href="https://england.shelter.org.uk/housing_advice/homelessness">
+                    Homelessness
+                  </a>
+                </li>
+                <li className="js-subnav js-active">
+                  <a href="https://england.shelter.org.uk/housing_advice/private_renting">
+                    Private renting
+                  </a>
+                </li>
+                <li className="js-subnav js-active">
+                  <a href="https://england.shelter.org.uk/housing_advice/tenancy_deposits">
+                    Tenancy deposits
+                  </a>
+                </li>
+                <li className="js-subnav js-active">
+                  <a href="https://england.shelter.org.uk/housing_advice/benefits">
+                    Benefits
+                  </a>
+                </li>
+                <li className="js-subnav js-active">
+                  <a href="https://england.shelter.org.uk/housing_advice/council_housing_association">
+                    Council housing
+                  </a>
+                </li>
+                <li className="js-subnav js-active">
+                  <a href="https://england.shelter.org.uk/housing_advice/eviction">
+                    Eviction
+                  </a>
+                </li>
+                <li className="js-subnav js-active">
+                  <a href="https://england.shelter.org.uk/housing_advice/repairs">
+                    Repairs
+                  </a>
+                </li>
+                <li className="js-subnav js-active">
+                  <a href="https://england.shelter.org.uk/housing_advice/money_problems_and_energy_costs">
+                    Energy costs
+                  </a>
+                </li>
+                <li className="js-subnav js-active">
+                  <a href="https://england.shelter.org.uk/housing_advice/repossession">
+                    Repossession
+                  </a>
+                </li>
+              </div>
+            </ul>
+          </li>
+          <li>
+            <a
+              href="https://england.shelter.org.uk/get_help"
+              id="js-main-nav-get-help"
+            >
+              Get help
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://england.shelter.org.uk/support_us"
+              id="test-1169133"
+            >
+              Support us
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://england.shelter.org.uk/what_we_do"
+              id="test-1385719"
+            >
+              What we do
+            </a>
+          </li>
 
-const Header = () => <Wrapper />;
+          <li className="secondary">
+            <a href="https://england.shelter.org.uk/professional_resources">
+              Professionals
+            </a>
+          </li>
+          <li className="secondary">
+            <a href="http://scotland.shelter.org.uk">Scotland</a>
+          </li>
+        </ul>
+      </MenuWrapper>
+    </>
+  );
+};
 
 export default Header;
--- src/components/header/styles.js
+++ src/components/header/styles.js
@@ -1,6 +1,65 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+import SVG from 'react-inlinesvg';
+import { buttonReset } from '../styled/buttons';
 
-export const Wrapper = styled.header`
-  padding: ${props => props.theme.spacing.large};
-  background: ${props => props.theme.palette.black};
+export const HeaderWrapper = styled.header`
+  display: flex;
+  justify-content: center;
+  width: 100%;
+  background: ${props => props.theme.palette.grey10};
+`;
+
+export const HeaderBar = styled.header`
+  display: flex;
+  justify-content: space-between;
+  max-width: 960px;
+  width: 100%;
+  margin: 0 auto;
+  padding: 0
+    ${props => (props.padding === false ? 0 : props.theme.spacing.standard)};
+  background: ${props => props.theme.palette.grey10};
+`;
+
+export const LogoWrapper = styled.a`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: ${props => props.theme.spacing.standard};
+  background: ${props => props.theme.palette.primary};
+`;
+
+export const Logo = styled(SVG)`
+  display: block;
+  width: 115px;
+`;
+
+export const MenuControl = styled.button`
+  display: block;
+  ${buttonReset}
+`;
+
+export const StyledMenuSVG = styled(SVG)`
+  display: block;
+  width: 20px;
+`;
+
+export const MenuWrapper = styled.div`
+  position: absolute;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  z-index: 10;
+  display: flex;
+  flex-direction: column;
+  width: 65%;
+  background: ${props => props.theme.palette.white};
+  box-shadow: -1px 0 14px rgba(0, 0, 0, 0.5);
+  transform: translateX(100%);
+  transition: transform 0.3s cubic-bezier(0, 0, 0.3, 1);
+
+  ${props =>
+    props.active &&
+    css`
+      transform: translateX(0);
+    `};
 `;
