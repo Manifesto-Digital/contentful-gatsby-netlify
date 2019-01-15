c0ab958c5676ef9cb0a3ea6e607615d7785b70e0
Toby Bushell <toby.bushell@manifesto.co.uk>
2019-01-15 12:23:04 +0000

    Padding added to page title

--- src/components/page-title/styles.js
+++ src/components/page-title/styles.js
@@ -2,4 +2,5 @@ import styled from 'styled-components';
 
 export const H1 = styled.h1`
   margin-bottom: ${props => props.theme.spacing.large};
+  padding-top: ${props => props.theme.spacing.standard};
 `;
