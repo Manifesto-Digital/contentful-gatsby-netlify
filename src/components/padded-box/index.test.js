e63d3df6ad5505ea9a0227615e06472eee02f5b6
Neil Dakeyne <neil_dakeyne@shelter.org.uk>
2019-01-18 17:47:20 +0000

    updated tests

---
+++ src/components/padded-box/index.test.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import {
+  snapshotComponent,
+  renderWithTheme,
+} from '../../../__tests__/helpers/index';
+import theme from '../theme/variables';
+
+import PaddedBox from '.';
+
+it('renders correctly with defaults', () => {
+  snapshotComponent(<PaddedBox />);
+});
+
+it('changes based on supplied props', () => {
+  const wrapper = renderWithTheme(<PaddedBox bg="grey45" padding="large" />);
+  expect(wrapper.toJSON()).toHaveStyleRule('padding', theme.spacing.large);
+  expect(wrapper.toJSON()).toHaveStyleRule(
+    'background-color',
+    theme.palette.grey45
+  );
+
+  const wrapper2 = renderWithTheme(
+    <PaddedBox bg="sanMarinoBlue" padding="small" />
+  );
+  expect(wrapper2.toJSON()).toHaveStyleRule('padding', theme.spacing.small);
+  expect(wrapper2.toJSON()).toHaveStyleRule(
+    'background-color',
+    theme.palette.sanMarinoBlue
+  );
+});
+
+it('it renders with shadow when supplied', () => {
+  const wrapper = renderWithTheme(<PaddedBox shadow />);
+  expect(wrapper.toJSON()).toHaveStyleRule('box-shadow', theme.boxshadow.small);
+  expect(wrapper.toJSON()).toHaveStyleRule(
+    'border-radius',
+    theme.borderradius.small
+  );
+});
