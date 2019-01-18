5c210e6c4e9a7e47c063892cafc1f7b499468cac
Neil Dakeyne <neil_dakeyne@shelter.org.uk>
2019-01-18 17:47:05 +0000

    Updated to theme vars and related components

--- src/components/padded-box/index.js
+++ src/components/padded-box/index.js
@@ -1,9 +1,17 @@
 import styled, { css } from 'styled-components';
 
-const getStyles = ({ bg = 'offWhite', padding = 'standard' }) => {
+const getStyles = ({
+  bg = 'offWhite',
+  padding = 'standard',
+  shadow = false,
+}) => {
   const styles = css`
     padding: ${props => props.theme.spacing[padding]};
     background-color: ${props => props.theme.palette[bg]};
+    box-shadow: ${props =>
+      shadow === false ? 0 : props.theme.boxshadow.small};
+    border-radius: ${props =>
+      shadow === false ? 0 : props.theme.borderradius.small};
   `;
 
   return styles;
