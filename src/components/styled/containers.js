2a0725f6e4fdad36435eba99b7fe581b51304f82
Neil Dakeyne <neil_dakeyne@shelter.org.uk>
2019-01-18 12:12:43 +0000

    corrected unintended change

--- src/components/styled/containers.js
+++ src/components/styled/containers.js
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 import { breakpoint } from '../theme/breakpoint';
 
 export const Container = styled.div`
-  max-width: 60em;
+  max-width: 960px;
   margin: 0 auto;
   padding: 0
     ${props => (props.padding === false ? 0 : props.theme.spacing.standard)};
