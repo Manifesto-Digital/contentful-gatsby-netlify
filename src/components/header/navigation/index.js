d5a4da888987ac5851a72cf4489da65962f0c420
Callam Williams <lhocdesign@gmail.com>
2019-01-29 11:39:26 +0000

    updated overlay functionality

--- src/components/header/navigation/index.js
+++ src/components/header/navigation/index.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import NavigationMenu from './navigation';
+import { Overlay } from '../../styled/overlay';
 
 import { MobileMenuClose, MenuSVG } from './styles-icons';
 import {
@@ -26,45 +27,52 @@ const Navigation = ({ pageData, active, openState }) => {
   };
 
   return (
-    <Wrapper active={active} ariaHidden={active}>
-      <MobileMenuClose
-        type="button"
-        onClick={openState}
+    <>
+      <Wrapper active={active} ariaHidden={active}>
+        <MobileMenuClose
+          type="button"
+          onClick={openState}
+          active={active}
+          aria-expanded={active}
+        >
+          <MenuSVG src={CloseSVG} />
+        </MobileMenuClose>
+        <Menus role="navigation" aria-label="Main menu">
+          {navigationItems && (
+            <MenuList role="menubar" aria-hidden="false">
+              {navigationItems.map((item, i) => (
+                <NavigationMenu
+                  key={i}
+                  id={item.id}
+                  pageData={item}
+                  menuOpen={activeMenu === item.id}
+                  setActiveMenu={updateActiveMenu}
+                />
+              ))}
+            </MenuList>
+          )}
+          {additionalLink && (
+            <AdditionalMenu role="navigation" aria-label="Secondary menu">
+              <Item>
+                <ItemLink internalLink={additionalLink[0]}>
+                  {additionalLink[0].menuLabel || additionalLink[0].title}
+                </ItemLink>
+              </Item>
+              <Item>
+                <ItemLink internalLink="https://scotland.shelter.org.uk/">
+                  Scotland
+                </ItemLink>
+              </Item>
+            </AdditionalMenu>
+          )}
+        </Menus>
+      </Wrapper>
+      <Overlay
         active={active}
-        aria-expanded={active}
-      >
-        <MenuSVG src={CloseSVG} />
-      </MobileMenuClose>
-      <Menus role="navigation" aria-label="Main menu">
-        {navigationItems && (
-          <MenuList role="menubar" aria-hidden="false">
-            {navigationItems.map((item, i) => (
-              <NavigationMenu
-                key={i}
-                id={item.id}
-                pageData={item}
-                menuOpen={activeMenu === item.id}
-                setActiveMenu={updateActiveMenu}
-              />
-            ))}
-          </MenuList>
-        )}
-        {additionalLink && (
-          <AdditionalMenu role="navigation" aria-label="Secondary menu">
-            <Item>
-              <ItemLink internalLink={additionalLink[0]}>
-                {additionalLink[0].menuLabel || additionalLink[0].title}
-              </ItemLink>
-            </Item>
-            <Item>
-              <ItemLink internalLink="https://scotland.shelter.org.uk/">
-                Scotland
-              </ItemLink>
-            </Item>
-          </AdditionalMenu>
-        )}
-      </Menus>
-    </Wrapper>
+        subMenuActive={!!activeMenu}
+        onClick={() => updateActiveMenu()}
+      />
+    </>
   );
 };
 
