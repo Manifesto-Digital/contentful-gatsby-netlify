2200bf4737c1b56296aebb1bccbf9e5de3cae5a6
Owen <o-w-jones@outlook.com>
2019-01-31 14:39:58 +0000

    Add styled container, data deconstruction and prop validation

--- src/components/testimonials/index.js
+++ src/components/testimonials/index.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { Container } from '../styled/containers';
 
-const Testimonials = () => (
-  <>
-    <p>My testimonials</p>
-  </>
-);
+const Testimonials = ({ data }) => {
+  const { headerText, testimonials } = data;
+
+  return (
+    <Container>
+      <h2>{headerText}</h2>
+    </Container>
+  );
+};
+
+Testimonials.propTypes = {
+  data: PropTypes.shape({
+    headerText: PropTypes.string.isRequired,
+  }),
+};
 
 export default Testimonials;
