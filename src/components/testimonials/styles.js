82f601f3d1f988f0fadacbfb10b5eb79da2eed48
Owen <o-w-jones@outlook.com>
2019-02-04 12:33:12 +0000

    Swap transform and absolute for minus margin solution

--- src/components/testimonials/styles.js
+++ src/components/testimonials/styles.js
@@ -2,13 +2,12 @@ import styled from 'styled-components';
 import { breakpoint } from '../theme/breakpoint';
 
 export const Container = styled.div`
-  position: relative;
-  margin-bottom: 10em;
+  margin-bottom: 2em;
 
   ${breakpoint.tablet`
+    margin-bottom: 0;
     display: flex;
     align-items: center;
-    margin-bottom: 0;
   `};
 `;
 
@@ -21,31 +20,20 @@ export const ImageContainer = styled.div`
 `;
 
 export const TextContainer = styled.div`
-  position: absolute;
-  bottom: -75%;
-  left: 0;
-  right: 0;
-  margin: auto;
+  position: relative;
   width: 90%;
   background-color: ${props =>
     (props.bg === 'Grey' && props.theme.palette.grey10) ||
     (props.bg === 'White' && props.theme.palette.white)};
   padding: 2em;
-
-  ${breakpoint.mobile`
-    bottom: -50%;
-  `};
-
-  ${breakpoint.mobileLand`
-    bottom: -25%;
-  `};
+  margin: -2em auto 0;
 
   ${breakpoint.tablet`
-    position: static;
     flex-grow: 1;
     flex-basis: 50%;
-    transform: ${({ loopIndex }) =>
-      loopIndex % 2 === 0 ? 'translateX(-2em)' : 'translateX(2em)'}
+    margin: 0;
+    margin-left: ${({ loopIndex }) => (loopIndex % 2 === 0 ? '-2em' : '2em')};
+    margin-right: ${({ loopIndex }) => (loopIndex % 2 === 0 ? '2em' : '-2em')};
   `};
 `;
 
