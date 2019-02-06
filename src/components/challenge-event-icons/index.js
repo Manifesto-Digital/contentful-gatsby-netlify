f11d51574020cfdfddc8c0ba1644f05ba9cd8d58
Owen <o-w-jones@outlook.com>
2019-02-06 11:07:03 +0000

    Challenge event icon data setup

--- src/components/challenge-event-icons/index.js
+++ src/components/challenge-event-icons/index.js
@@ -1,10 +1,29 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import SVG from 'react-inlinesvg';
+import { Container } from '../styled/containers';
+import { consistentString } from '../../utils/content-formatting';
+import iconSrc from '../styled/iconSrc';
 
 const ChallengeEventIcons = ({ data, insideContainer }) => {
   const { headerText, theme, eventIcons } = data;
 
-  return <></>;
+  return (
+    <div bgColour={theme}>
+      <Container padding={!insideContainer}>
+        {headerText !== null && <h2>{headerText}</h2>}
+
+        <div>
+          {eventIcons.map((eventIcon, key) => (
+            <div key={key}>
+              <SVG src={iconSrc(consistentString(eventIcon))} />
+              {eventIcon}
+            </div>
+          ))}
+        </div>
+      </Container>
+    </div>
+  );
 };
 
 ChallengeEventIcons.propTypes = {
