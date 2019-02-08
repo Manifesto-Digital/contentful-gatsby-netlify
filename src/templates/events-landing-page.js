c220e1889a11fbac63d1ba01d2642dc2a11289f1
Owen <o-w-jones@outlook.com>
2019-02-08 15:39:12 +0000

    Title, intro text and container formatting

--- src/templates/events-landing-page.js
+++ src/templates/events-landing-page.js
@@ -1,12 +1,30 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { graphql } from 'gatsby';
+import styled from 'styled-components';
 import Layout from '../components/layout';
 // Components
 import PageTitle from '../components/page-title';
 import RichText from '../components/rich-text';
 // Styles
-import { Container } from '../components/styled/containers';
+import { Container, TwoThirds } from '../components/styled/containers';
+import theme from '../components/theme/variables';
+
+const Wrapper = styled.div`
+  background-color: ${theme.palette.grey10};
+  padding: ${theme.spacing.medium} 0 ${theme.spacing.standard};
+  margin: ${theme.spacing.large} 0 ${theme.spacing.standard};
+  position: relative;
+`;
+
+const SectionTag = styled.div`
+  display: inline-block;
+  position: absolute;
+  top: -25px;
+  background-color: ${theme.palette.primary};
+  color: ${theme.palette.white};
+  padding: ${theme.spacing.small};
+`;
 
 const EventsLandingPage = ({ data }) => {
   const {
@@ -23,6 +41,14 @@ const EventsLandingPage = ({ data }) => {
         <Container>
           {topTextSection && <RichText richText={topTextSection} />}
         </Container>
+        <Wrapper>
+          <Container>
+            <TwoThirds>
+              <SectionTag>Featured events</SectionTag>
+              <p>hello</p>
+            </TwoThirds>
+          </Container>
+        </Wrapper>
       </article>
     </Layout>
   );
