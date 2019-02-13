75080ccf3e90b9d86d807f91ad3ad75d9542b4e1
duncan heron <duncanuk@gmail.com>
2019-02-13 16:27:53 +0000

    moves to async and fs/promises

--- __netlify__/redirects.js
+++ __netlify__/redirects.js
@@ -4,45 +4,75 @@ require('dotenv').config({
 
 const contentful = require('contentful');
 const fs = require('fs');
-const fsPromises = require('fs').promises;
+const fsPromises = require('fs/promises');
 
 const client = contentful.createClient({
   space: process.env.ctfl_spaceId,
   accessToken: process.env.ctfl_accessToken,
 });
 
+const filepath = `${__dirname}/../public/_redirects`;
+const filepathFixedRedirects = `${__dirname}/fixed_redirects.txt`;
+
 client
   .getEntries({
     content_type: 'pageAssemblyContentPage',
   })
-  .then(Entries => {
+  .then(async Entries => {
     const generateRedirects = async () => {
-      const filepath = `${__dirname}/../public/_redirects`;
-      try {
-        await fsPromises.truncate(filepath, 4);
-        console.log('redirects emptied');
-        await fsPromises.appendFile(filepath, '#auto generated redirects \n\n');
-        console.log('header added');
-        let i = 1; // for now we need this to generate unique urls
-        Object.keys(Entries.items).map(key => {
-          const writeRedirectsFile = async slug => {
-            await fsPromises.appendFile(
-              filepath,
-              `/this-should-redirect${i}              /${slug} \n`
-            );
-          };
-          const { slug } = Entries.items[key].fields;
+      await fsPromises.truncate(filepath);
+      console.log('redirects emptied');
+      await fsPromises.appendFile(filepath, '# auto generated redirects \n\n');
+      console.log('header added');
+      Object.keys(Entries.items).map(key => {
+        const { slug } = Entries.items[key].fields;
 
-          writeRedirectsFile(slug);
-          console.log(`${slug} added.`);
-          i += 1; // for now we need this to generate unique urls
-        });
-      } catch (error) {
-        console.log(error);
-      }
+        const writeRedirectsFile = async () => {
+          await fsPromises.appendFile(
+            filepath,
+            `/this-should-redirect${key}              /${slug} \n`
+          );
+        };
+
+        writeRedirectsFile(slug);
+
+        console.log(`${slug} added.`);
+        return true;
+      });
     };
-    generateRedirects();
+    await generateRedirects().catch(error => {
+      console.log(error);
+      process.exit(1);
+    });
+
+    const readFile = async () => {
+      let fileHandle = null;
+      fileHandle = await fsPromises.open(filepathFixedRedirects, 'r+');
+      const content = await fsPromises.readFile(fileHandle, 'utf-8');
+
+      if (fileHandle) {
+        await fileHandle.close();
+      }
+      console.log(content);
+      return content;
+    };
+
+    const writeStaticRedirectsFile = async staticRedirects => {
+      await fsPromises.appendFile(filepath, '# static redirects generated\n\n');
+      await fsPromises.appendFile(filepath, staticRedirects);
+    };
+
+    const fileContents = await readFile().catch(error => {
+      console.log(error);
+      process.exit(1);
+    });
+
+    writeStaticRedirectsFile(fileContents).catch(error => {
+      console.log(error);
+      process.exit(1);
+    });
   })
   .catch(error => {
     console.log(error);
+    process.exit(1);
   });
