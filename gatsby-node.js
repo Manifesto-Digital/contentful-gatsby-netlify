377501bb9ac0b541776d99b7e039b19557fcc959
Owen <o-w-jones@outlook.com>
2019-02-13 12:24:31 +0000

    Build page generation for event category pages

--- gatsby-node.js
+++ gatsby-node.js
@@ -3,6 +3,7 @@ const createContentPages = require('./create-pages/content-pages.js');
 const createPressReleasePages = require('./create-pages/press-release-pages.js');
 const createEventPages = require('./create-pages/event-pages.js');
 const createEventsLandingPages = require('./create-pages/events-landing-pages.js');
+const createEventCategoryPages = require('./create-pages/event-category-pages.js');
 
 exports.createPages = async ({ graphql, actions }) => {
   const { createPage } = actions;
@@ -12,4 +13,5 @@ exports.createPages = async ({ graphql, actions }) => {
   createPressReleasePages(graphql, createPage);
   createEventPages(graphql, createPage);
   createEventsLandingPages(graphql, createPage);
+  createEventCategoryPages(graphql, createPage);
 };
---
+++ src/templates/event-category-page.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { graphql } from 'gatsby';
+import Layout from '../components/layout';
+// Components
+import PageTitle from '../components/page-title';
+import RichText from '../components/rich-text';
+// Styles
+import { Container, TwoThirds } from '../components/styled/containers';
+
+const EventCategoryPage = ({ data }) => {
+  const {
+    pageName,
+    strapline,
+    summary,
+    featuredEvent,
+  } = data.contentfulPageAssemblyEventCategory;
+
+  const events = data.allContentfulTopicEvent.edges;
+
+  return (
+    <Layout>
+      <article>
+        <p>hello world</p>
+      </article>
+    </Layout>
+  );
+};
+
+EventCategoryPage.propTypes = {
+  data: PropTypes.shape({
+    contentfulPageAssemblyEventCategory: PropTypes.object,
+    allContentfulTopicEvent: PropTypes.object,
+  }),
+};
+
+export default EventCategoryPage;
+
+export const eventCategoryPageQuery = graphql`
+  query eventCategoryPageTemplateQuery($slug: String!, $type: String!) {
+    contentfulPageAssemblyEventCategory(slug: { eq: $slug }) {
+      pageName
+      strapline
+      summary {
+        childContentfulRichText {
+          html
+        }
+      }
+      featuredEvent {
+        eventName
+        displayLocation
+        eventDisplayDate
+        distance
+      }
+    }
+
+    allContentfulTopicEvent(filter: { eventType: { eq: $type } }) {
+      edges {
+        node {
+          eventName
+          displayLocation
+          eventDisplayDate
+        }
+      }
+    }
+  }
+`;
