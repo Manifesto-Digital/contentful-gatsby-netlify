300d263e29585db6a36d54a6d8515b50a9ee054f
Owen <o-w-jones@outlook.com>
2019-02-14 17:04:25 +0000

    Write tests

---
+++ src/components/event-list-card/index.test.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { Link } from 'gatsby';
+import { createFactory, createImage } from '../../utils/test-factories';
+import {
+  snapshotComponent,
+  mountWithTheme,
+} from '../../../__tests__/helpers/index';
+import EventListCard from './index';
+
+export const createEventListCard = createFactory({
+  slug: 'my-slug',
+  event: {
+    thumbnailImage: createImage(),
+    eventName: 'My Event',
+    displayLocation: 'round the corner',
+    eventDisplayDate: 'Yesterday',
+  },
+});
+
+it('renders correctly', () => {
+  const mockData = createEventListCard();
+
+  snapshotComponent(<EventListCard data={mockData} />);
+});
+
+it('displays the specified image correctly', () => {
+  const mockData = createEventListCard();
+  const wrapper = mountWithTheme(<EventListCard data={mockData} />);
+  expect(wrapper.find('img').prop('src')).toContain(
+    mockData.event.thumbnailImage.file.url
+  );
+});
+
+it('sets the correct CTA links', () => {
+  const mockData = createEventListCard({ slug: 'my-test-slug' });
+  const wrapper = mountWithTheme(<EventListCard data={mockData} />);
+
+  expect(
+    wrapper
+      .find(Link)
+      .at(0)
+      .prop('to')
+  ).toContain('my-test-slug');
+});
