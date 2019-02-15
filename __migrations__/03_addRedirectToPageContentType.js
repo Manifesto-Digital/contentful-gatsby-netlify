d189537c96e076ed5735e6c275616ec63d5b91fc
duncan heron <duncanuk@gmail.com>
2019-02-15 11:38:57 +0000

    create migrations for version, redirect and add redirect to default page content type

---
+++ __migrations__/03_addRedirectToPageContentType.js
@@ -0,0 +1,25 @@
+module.exports = function(migration) {
+  const pageAssemblyContentPage = migration.editContentType(
+    'pageAssemblyContentPage'
+  );
+  pageAssemblyContentPage
+    .createField('redirects')
+    .name('Redirects')
+    .type('Array')
+    .localized(false)
+    .required(false)
+    .validations([])
+    .disabled(false)
+    .omitted(false)
+    .items({
+      type: 'Link',
+
+      validations: [
+        {
+          linkContentType: ['redirect'],
+        },
+      ],
+
+      linkType: 'Entry',
+    });
+};
