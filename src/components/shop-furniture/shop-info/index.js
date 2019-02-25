3d0e3c5498b65868846d6193ccd2e35b3fc568cc
Toby Bushell <toby.bushell@manifesto.co.uk>
2019-02-25 16:06:02 +0000

    console.log removed

--- src/components/shop-furniture/shop-info/index.js
+++ src/components/shop-furniture/shop-info/index.js
@@ -7,54 +7,50 @@ import iconSrc from '../../../utils/iconSrc';
 import { Wrapper, Flex, Flex25, Content, Icon, Heading } from './styles';
 import { Container } from '../../styled/containers';
 
-const ShopInfo = ({ address, openingHours, parking, disabledAccess }) => {
-  console.log('woo');
-
-  return (
-    <Wrapper>
-      <Container>
-        <Flex>
-          <Flex25>
-            <Content>
-              <Heading>
-                <Icon src={iconSrc('map-marker')} />
-                Address
-              </Heading>
-              <RichText richText={address} />
-            </Content>
-          </Flex25>
-          <Flex25>
-            <Content>
-              <Heading>
-                <Icon src={iconSrc('clock')} />
-                Opening hours
-              </Heading>
-              <RichText richText={openingHours} />
-            </Content>
-          </Flex25>
-          <Flex25>
-            <Content>
-              <Heading>
-                <Icon src={iconSrc('car')} />
-                Parking
-              </Heading>
-              <RichText richText={parking} />
-            </Content>
-          </Flex25>
-          <Flex25>
-            <Content>
-              <Heading>
-                <Icon src={iconSrc('wheelchair')} />
-                Disabled access
-              </Heading>
-              <RichText richText={disabledAccess} />
-            </Content>
-          </Flex25>
-        </Flex>
-      </Container>
-    </Wrapper>
-  );
-};
+const ShopInfo = ({ address, openingHours, parking, disabledAccess }) => (
+  <Wrapper>
+    <Container>
+      <Flex>
+        <Flex25>
+          <Content>
+            <Heading>
+              <Icon src={iconSrc('map-marker')} />
+              Address
+            </Heading>
+            <RichText richText={address} />
+          </Content>
+        </Flex25>
+        <Flex25>
+          <Content>
+            <Heading>
+              <Icon src={iconSrc('clock')} />
+              Opening hours
+            </Heading>
+            <RichText richText={openingHours} />
+          </Content>
+        </Flex25>
+        <Flex25>
+          <Content>
+            <Heading>
+              <Icon src={iconSrc('car')} />
+              Parking
+            </Heading>
+            <RichText richText={parking} />
+          </Content>
+        </Flex25>
+        <Flex25>
+          <Content>
+            <Heading>
+              <Icon src={iconSrc('wheelchair')} />
+              Disabled access
+            </Heading>
+            <RichText richText={disabledAccess} />
+          </Content>
+        </Flex25>
+      </Flex>
+    </Container>
+  </Wrapper>
+);
 
 ShopInfo.propTypes = {
   address: PropTypes.shape(richTextPropTypes).isRequired,
