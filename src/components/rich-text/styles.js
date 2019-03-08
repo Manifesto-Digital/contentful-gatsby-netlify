8c27d269b50c3c1e243463e05bb97f6ea50360ce
Callam Williams <lhocdesign@gmail.com>
2019-03-08 13:47:38 +0000

    made bullet points square, nested ones are outlined

--- src/components/rich-text/styles.js
+++ src/components/rich-text/styles.js
@@ -8,4 +8,40 @@ export const Wrapper = styled.div`
       padding: ${theme.spacing.standard};
       margin-bottom: ${theme.spacing.standard};
     `}
+
+  /**
+  * Square bulletpoints, outlined for nested lists
+  */
+  ul {
+    padding-left: 0;
+    list-style: none;
+    li {
+      position: relative;
+      padding-left: ${({ theme }) => theme.spacing.large};
+
+      &::before {
+        content: ' ';
+        position: absolute;
+        top: 8px;
+        left: ${({ theme }) => theme.spacing.small};
+        display: block;
+        width: ${({ theme }) => theme.spacing.small};
+        height: ${({ theme }) => theme.spacing.small};
+        background: ${({ theme }) => theme.palette.black};
+      }
+
+      ul {
+        padding-left: 0;
+        list-style: none;
+        li:before {
+          background: transparent;
+          border: 1px solid ${({ theme }) => theme.palette.black};
+        }
+      }
+    }
+  }
+
+  ol li::before {
+    min-width: 10px;
+  }
 `;
