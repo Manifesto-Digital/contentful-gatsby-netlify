5a7df19166116d3ffa6c984be5030a51e8c9ece2
Callam Williams <lhocdesign@gmail.com>
2019-03-12 13:16:44 +0000

    finished bounce card

--- src/components/bounce-card/index.js
+++ src/components/bounce-card/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import LinkHandler from '../link-handler';
 import Image from '../image';
-import Wrapper from './styles';
+import { Wrapper } from './styles';
 import ScotlandOnly from '../../images/scotland_only.png';
 
 const BounceCard = () => (
@@ -17,14 +17,10 @@ const BounceCard = () => (
       height={76}
     />
     <div>
-      Housing laws differ between Scotland and England. This content applies to
-      Scotland only.
-      <LinkHandler
-        externalUrl={{
-          URL: 'https://england.shelter.org.uk/',
-          newTab: true,
-        }}
-      >
+      Housing laws differ between Scotland and England. {'\n'}This content
+      applies to <strong>Scotland only.</strong>
+      {'\n'}
+      <LinkHandler externalUrl="https://england.shelter.org.uk/">
         Get advice if you're England
       </LinkHandler>
     </div>
--- src/components/bounce-card/styles.js
+++ src/components/bounce-card/styles.js
@@ -2,6 +2,21 @@ import styled from 'styled-components';
 
 export const Wrapper = styled.div`
   display: flex;
-  background-color: ${({ theme }) => theme.palette.grey10};
+  background-color: ${({ theme }) => theme.palette.offWhite};
   padding: ${({ theme }) => theme.spacing.standard};
+  font-size: ${({ theme }) => theme.fontsize.tiny};
+
+  img {
+    width: 45px;
+    height: 76px;
+    margin-right: ${({ theme }) => theme.spacing.small};
+  }
+
+  div {
+    white-space: pre-line;
+  }
+
+  a {
+    text-decoration: none;
+  }
 `;
