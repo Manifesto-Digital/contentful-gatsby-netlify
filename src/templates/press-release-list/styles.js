d8a67f3fb0948ff4976f43f35207d126520144cb
Toby Bushell <toby.bushell@gmail.com>
2019-04-11 17:01:58 +0100

    Slug queried from press-release listing page in contentful. Folders created for template files for press releases to prevent styled components living in the same file as the template

---
+++ src/templates/press-release-list/styles.js
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+
+export const List = styled.div`
+  background: ${({ theme }) => theme.palette.grey10};
+  padding-top: ${({ theme }) => theme.spacing.large};
+`;
