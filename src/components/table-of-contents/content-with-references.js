479785d345de5319efd9bff97d4b38625df6e6b6
Toby Bushell <toby.bushell@gmail.com>
2019-07-11 15:11:14 +0100

    Merged in Legal footnotes

--- src/components/table-of-contents/content-with-references.js
+++ src/components/table-of-contents/content-with-references.js
@@ -1,44 +1,17 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
-import RichText from '../rich-text';
 import { Block, BlockContent, BlockTitle } from './styles';
-import { bracketsToLink } from '../../utils/rich-text-formatting/bracketsToLink';
-import { bracketsToArray } from '../../utils/rich-text-formatting/bracketsToArray';
+import { Wrapper } from '../rich-text/styles';
 
-const ContentWithReferences = ({ tableOfContents, updateReferenceList }) => {
-  const [contentWithReferences, createReferenceContent] = useState([]);
-
-  useEffect(() => {
-    const convertBracketsToLink = text => {
-      const formattedContent = bracketsToLink(text);
-      createReferenceContent(formattedContent);
-    };
-
-    const convertBracketsToArray = text => {
-      const referenceArray = bracketsToArray(text);
-      updateReferenceList(referenceArray);
-    };
-    if (!tableOfContents) return;
-    convertBracketsToLink(tableOfContents);
-    convertBracketsToArray(tableOfContents);
-  }, [tableOfContents, updateReferenceList]);
-
-  if (!tableOfContents) return null;
+const ContentWithReferences = ({ contents }) => {
+  if (!contents) return null;
 
   return (
     <Block>
-      {tableOfContents.map((item, i) => (
+      {contents.map((item, i) => (
         <BlockContent key={i}>
           <BlockTitle id={`title-${i}`}>{item.title}</BlockTitle>
-          {contentWithReferences[i] && (
-            <RichText
-              richText={{
-                childContentfulRichText: {
-                  html: contentWithReferences[i],
-                },
-              }}
-            />
-          )}
+          <Wrapper>{item.textContent}</Wrapper>
         </BlockContent>
       ))}
     </Block>
@@ -46,8 +19,7 @@ const ContentWithReferences = ({ tableOfContents, updateReferenceList }) => {
 };
 
 ContentWithReferences.propTypes = {
-  tableOfContents: PropTypes.array.isRequired,
-  updateReferenceList: PropTypes.func.isRequired,
+  contents: PropTypes.array.isRequired,
 };
 
 export default ContentWithReferences;
