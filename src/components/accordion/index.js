bbbd90eeb7890f29d49ac3f51b6e95ac32df808b
Toby Bushell <toby.bushell@gmail.com>
2019-07-23 17:18:31 +0100

    Updated DataLayer for accordion

--- src/components/accordion/index.js
+++ src/components/accordion/index.js
@@ -15,7 +15,7 @@ function Accordion({ header, children, className, active, id, footer, order }) {
       event: 'accordionInteraction',
       accordionTitle: `${header}`,
       accordionPosition: `${order}`,
-      accordionOpen: isOpen
+      accordionOpen: `${isOpen}`,
     };
     sendEvent(analyticsData);
   };
